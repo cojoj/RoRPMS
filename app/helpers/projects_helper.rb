1cc160aa9c50678021066ff9408d0019d7d02089
Mateusz Zając <zajac.mateusz@icloud.com>
2013-12-26 23:22:03 +0100

    Added helper and partial

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+  def projects(status)
+    waiting = Array.new
+    @projects.each do |p|
+      if p.status == status
+        waiting.push(p)
+      end
+    end
+    return waiting
+  end
 end
